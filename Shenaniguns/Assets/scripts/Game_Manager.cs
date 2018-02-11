9f973e486088417a733b1baf6b205d1ec52829bc
Chris Duffy <christopher.b.duffy@gmail.com>
2018-02-11 12:56:45 -0800

    Jump & load arena
    Added buggy jump function. player.IsGrounded() needs work. added a test arena. its really big. Added sceneloader to game_controller
    
    run mainscene then press 1 to load arena and 2 to load players

---
+++ Shenaniguns/Assets/scripts/Game_Manager.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+
+public class Game_Manager : MonoBehaviour
+{
+    public static Game_Manager instance;
+    private List<GameObject> players = new List<GameObject>();
+    private List<GameObject> cameras = new List<GameObject>();
+
+    //splitscreen viewports
+    private Rect full = new Rect(0.0f, 0.0f, 1.0f, 1.0f);
+    private Rect top = new Rect(0.0f, 0.5f, 1.0f, 0.5f);
+    private Rect bot = new Rect(0.0f, 0.0f, 1.0f, 0.5f);
+    private Rect topLeft = new Rect(0.0f, 0.5f, 0.5f, 0.5f);
+    private Rect topRight = new Rect(0.5f, 0.5f, 0.5f, 0.5f);
+    private Rect botLeft = new Rect(0.0f, 0.0f, 0.5f, 0.5f);
+    private Rect botRight = new Rect(0.5f, 0.0f, 0.5f, 0.5f);
+
+    //this pretty much just enforces singletons
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != this)
+        {
+            Destroy(gameObject);
+        }
+        DontDestroyOnLoad(gameObject);
+
+    }
+
+    void StartGame(int numPlayers, string arena) {
+
+        SceneManager.LoadScene(arena, LoadSceneMode.Single);
+    }
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Player1Start"))
+        {
+            StartGame(1, "testArena");
+        }
+        if (Input.GetButtonDown("Player2Start"))
+        {
+            CreatePlayer(1);
+            CreatePlayer(2);
+        }
+    }
+
+    public GameObject CreatePlayer(int playerNum)
+    {
+        GameObject player = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        player.AddComponent(System.Type.GetType("Player"));
+        player.transform.position = new Vector3(0, 5, 0);
+        player.AddComponent<Rigidbody>();
+        player.GetComponent<Rigidbody>().freezeRotation = true;
+        player.GetComponent<Player>().Init(playerNum);
+        BuildPlayerCamera(player);
+        return player;
+    }
+
+    public void BuildPlayerCamera(GameObject player) {
+        GameObject camera = new GameObject();
+        camera.AddComponent<Camera>();
+        camera.GetComponent<Camera>().transform.position = new Vector3(0, 10, -10);
+        camera.transform.LookAt(player.transform);
+        camera.transform.parent = player.transform;
+        instance.cameras.Add(camera);
+        CalcSplitScreens();
+    }
+
+    public void CalcSplitScreens() {
+
+        switch (instance.cameras.Count) {
+            case 1:
+                instance.cameras[0].GetComponent<Camera>().rect = full;
+                break;
+            case 2:
+                instance.cameras[0].GetComponent<Camera>().rect = top;
+                instance.cameras[1].GetComponent<Camera>().rect = bot;
+                break;
+            case 3:
+                instance.cameras[0].GetComponent<Camera>().rect = top;
+                instance.cameras[1].GetComponent<Camera>().rect = botLeft;
+                instance.cameras[2].GetComponent<Camera>().rect = botRight;
+                break;
+            case 4:
+                instance.cameras[0].GetComponent<Camera>().rect = topLeft;
+                instance.cameras[1].GetComponent<Camera>().rect = topRight;
+                instance.cameras[2].GetComponent<Camera>().rect = botLeft;
+                instance.cameras[3].GetComponent<Camera>().rect = botRight;
+                break;
+            default:
+                break;
+        }
+        
+    }
+}
---
+++ Shenaniguns/Assets/scripts/Player.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+    int playerNum;
+    public void Init(int index) {
+        playerNum = index;
+    }
+
+
+	void Update () {
+        var x = Input.GetAxis("j"+playerNum+"x1") * Time.deltaTime * 150.0f;
+        var z = Input.GetAxis("j"+playerNum+"y1") * Time.deltaTime * 3.0f;
+
+        transform.Rotate(0, x, 0);
+        transform.Translate(0, 0, -z);
+
+        if (Input.GetButtonDown("j"+playerNum+"a")) {
+            if (IsGrounded())
+            {
+                GetComponent<Rigidbody>().velocity = new Vector3(0, 10, 0);
+            }
+        }
+    }
+
+    bool IsGrounded(){
+        if (GetComponent<Rigidbody>().velocity.y == 0)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}

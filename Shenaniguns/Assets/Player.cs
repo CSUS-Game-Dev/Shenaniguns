6eea5dd737e0742012eaf49bfcf402fc1fc73800
Chris Duffy <christopher.b.duffy@gmail.com>
2018-02-11 00:38:10 -0800

    First Commit
    GameManager and PrettyMuch Empty Player Class. It adds players and calculates splitscreen. Breaks after 4. You need a gamepad or two to play. use 1 and 2 to add players

---
+++ Shenaniguns/Assets/Player.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+    int playerNum;
+    public void Init(int index) {
+        playerNum = index;
+    }
+
+
+	void Update () {
+        var x = Input.GetAxis("j"+playerNum+"x1") * Time.deltaTime * 150.0f;
+        var z = Input.GetAxis("j"+playerNum+"y1") * Time.deltaTime * 3.0f;
+
+        transform.Rotate(0, x, 0);
+        transform.Translate(0, 0, z);
+    }
+}

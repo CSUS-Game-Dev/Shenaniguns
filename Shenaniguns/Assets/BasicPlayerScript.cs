0ebe8f50ef20a6aac187449e232e4c9b4a7bf0e1
Bryce Collins <brycecollins0@gmail.com>
2018-02-25 17:33:35 -0800

    Working with basic character movement

---
+++ Shenaniguns/Assets/BasicPlayerScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicPlayerScript : MonoBehaviour {
+
+	[SerializeField] public float moveSpeed;
+	private CharacterController controller;
+	// Use this for initialization
+	void Start () {
+		controller = GetComponent<CharacterController>();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		move();
+	}
+
+	public void move(){
+		controller.Move(new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical")));
+	}
+
+}
